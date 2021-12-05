259724f78165cc1e1f691632aac52848a44394bb
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-05 08:37:29 -0300

    Comentários em PHP usando #, // e /* */ para múltiplas linhas.

---
+++ PHP/4_comentarios/index.php
@@ -0,0 +1,13 @@
+<?php
+   
+   //Comentário de uma linha
+   #Comentario de uma linha
+   echo "Comentário de apenas uma linha, representados por duas barras ou por cerquilha // ou #. <br>";
+
+   /*
+    Comentário 
+    com múltiplas
+     linhas
+   */
+  echo "Comentários com múltiplas linhas, representada por uma barra e um asterisco /* */";
+?>
