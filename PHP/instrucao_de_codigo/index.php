074216c0007d2a37f093d219f94945a4e880989b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-05 07:30:33 -0300

    Instruções de códigos como variáveis, métodos e atributos, usa-se o ponto e vírgula no final.Para instruções de controle, usa-se chave para abrir e fechar.

---
+++ PHP/instrucao_de_codigo/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$numero1 = 10;
+$numero2 = 5;
+
+echo "Instrução de uma linha, usando ponto e vírgula";
+echo "<br>";
+
+if($numero1 > $numero2){
+    echo "Instrução de múltiplas linhas, usa-se chaves";
+}
+
+?>
