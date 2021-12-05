d051d662d9de02c417388ca7e589ca67086f36ee
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-04 23:41:05 -0300

    Case sensitive: São aplicadas em variáveis, letras maiúsculas são diferentes de letras minúsculas na memória.

---
+++ PHP/1_case_sensitive/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Não é case sensitive
+echo  "teste <br>";
+ECHO "testando <br>";
+echo "testando de novo <br>";
+
+//É case sensitive
+$nome = "João";
+$NOME = "testando outra vez";
+
+echo $nome;
+echo "<br>";
+echo $NOME;
+
+?>
