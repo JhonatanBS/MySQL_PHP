9ed4747cfe8021b499bca8c7721ed2225e8e9a24
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-07 13:02:44 -0300

    Array associativo: Possue uma chave e um valor, geralmente usamos string em chaves.

---
+++ PHP/Tipos_de_dados/10_array_associativo/index.php
@@ -0,0 +1,14 @@
+<?php
+  /*Array associativo; Possue uma chave e um valor*/
+  
+  $array = [ "nome" => "João", "cor" => "azul", "cachorro" => "pitbull"];
+
+  echo $array["nome"];
+  echo "<br>";
+  echo $array["cor"];
+  echo "<br>";
+  
+  $arrayAss = ["produto" => "arroz", "sol" => "quente", "verdadeiro" => true];
+  
+  print_r($arrayAss);
+?>
