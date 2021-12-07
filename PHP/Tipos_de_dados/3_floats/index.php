ff20ed9dee90ef5e4be1e466ba4d0fdb5ebe81c4
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-07 09:43:41 -0300

    float: São números flutuantes ou números com casas decimais.

---
+++ PHP/Tipos_de_dados/3_floats/index.php
@@ -0,0 +1,12 @@
+<?php
+   /*float: Números que possuem casas decimais*/
+   
+   $numero = 3.14;
+   echo $numero;
+   echo "<br>";
+   echo 1.5 + 20.7;
+   echo "<br>";
+   echo 4.5 * 10.2;
+   echo "<br>";
+   echo 15 + 2.5;
+?>
