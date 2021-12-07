d0dd5baf2624782f6825e5b64f3a931adb01c50c
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-07 11:06:19 -0300

    Arrays: São vetores que são representados por colchetes [], e aceitam qualquer tipo de dado.

---
+++ PHP/Tipos_de_dados/9_arrays/index.php
@@ -0,0 +1,13 @@
+<?php
+
+   $array = [15,152,30,60];
+
+   print_r($array);
+   echo "<br>";
+   echo $array[0];
+
+   $varios = ["Marcos", 100, 25.3, true,false];
+   echo "<br>";
+   print_r($varios);
+
+?>
