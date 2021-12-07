0bcde47d9d35f9e210f9031e0ca8964cbe3de435
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-07 10:45:12 -0300

    Booleano: Retorna true ou false.

---
+++ PHP/Tipos_de_dados/7_booleanos/index.php
@@ -0,0 +1,18 @@
+<?php
+   echo true;//Retorna 1
+   echo "<br>";
+   echo false;// Retorna nada
+
+   $aberto = true;
+
+   if($aberto){
+       echo "A porta está aberta";
+   }
+
+   echo "<br>";
+
+   if(10 > 5){
+       echo "É verdadeiro";
+   }
+
+?>
