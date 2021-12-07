93f5d4fc251287ec502a82513e10a54c8cc580f9
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-07 10:55:04 -0300

    Correção dos comentários do checando_booleano.

--- PHP/Tipos_de_dados/8-checando_booleano/index.php
+++ PHP/Tipos_de_dados/8-checando_booleano/index.php
@@ -1,5 +1,5 @@
 <?php
-  /*is_string: Verifica se o dado é string, retornando true ou false*/
+  /*is_bool: Verifica se o dado é um booleano, retornando true ou false*/
   if(is_bool(10 > 5)){
       echo "É um booleano <br>";
   }
