c50a4b08aa5b0c7126db04f626d98aa34eb728db
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-07 09:57:55 -0300

    Strings: Tipo de dado que é representado por aspas simples ou duplas, no caso de inserção de dados, usa-se asplas duplas.

---
+++ PHP/Tipos_de_dados/5_strings/index.php
@@ -0,0 +1,10 @@
+<?php
+   
+   echo "Texto com aspas duplas <br>";
+   echo 'Texto com aspas simples <br>';
+
+   $idade = 20;
+   /*Aspas duplas aceitam o tipo de dado*/
+   echo "Eu tenho $idade anos <br>";
+   echo 'Eu tenho $idade anos';
+?>
