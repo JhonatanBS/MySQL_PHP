b3be8ca0b788243241816c95c9439ddcb2bb5419
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-01-28 11:46:13 -0300

    Criando pedidos das pizzas com PHP e inserindo no Banco de dados.

--- Pizzaria_do_João/process/pizza.php
+++ Pizzaria_do_João/process/pizza.php
@@ -34,8 +34,45 @@
             $_SESSION["status"] = "warning";
 
         }else{
-            echo "passou da validação";
-            exit;
+            
+            // Salvando borda e massa na pizza
+            $stmt = $conn->prepare("INSERT INTO pizzas (borda_id, massa_id) VALUES (:borda, :massa)");
+
+            // Filtrando inputs
+            $stmt->bindParam(":borda", $borda, PDO::PARAM_INT);
+            $stmt->bindParam(":massa", $massa, PDO::PARAM_INT);
+
+            $stmt->execute();
+
+            // Resgatando último id da ultima pizza
+            $pizzaId = $conn->lastInsertId();
+
+            $stmt = $conn->prepare("INSERT INTO pizza_sabor (pizza_id, sabor_id) VALUES (:pizza,:sabor)");
+
+            foreach($sabores as $sabor){
+                //filtrando os inputs
+                $stmt->bindParam(":pizza", $pizzaId, PDO::PARAM_INT);
+                $stmt->bindParam(":sabor", $sabor, PDO::PARAM_INT);
+
+                $stmt->execute();
+
+            }
+
+            // Criar o pedido da pizza
+            $stmt = $conn->prepare("INSERT INTO pedidos (pizza_id, status_id) VALUES (:pizza, :status)");
+
+            // Status -> sempre inicia com 1, que é em produção
+            $statusId = 1;
+
+            // Filtrar os inputs
+            $stmt->bindParam(":pizza", $pizzaId);
+            $stmt->bindParam(":status", $statusId);
+
+            $stmt->execute();
+
+            //Exibir mensagem de sucesso
+            $_SESSION["msg"] = "Pedido realizado com sucesso";
+            $_SESSION["status"] = "success";
         }
 
         // Retorna para a página Inicial
