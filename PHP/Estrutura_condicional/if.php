a1d979d376e0cf28eada8657b80f6d621a16e921
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-02-07 18:49:21 -0300

    Estrutura condicional usando o if.

---
+++ PHP/Estrutura_condicional/if.php
@@ -0,0 +1,17 @@
+<?php
+  // if => Se
+  if( 10 > 7){
+      echo "Se 10 for maior que 7, esse if vai ser mostrado na tela! <br>";
+  }
+
+  if( 7 < 10){
+      echo "Se 7 for menor que 10, esse esse if vai ser mostrado <br>";
+  }
+
+  $a = 100;
+  $b = 150;
+
+  if($a === 100 && $b > $a){
+      echo "Verdadeiro";
+  }
+?>
