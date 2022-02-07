3ac318f5865c7fe20bf4e38ebae974a1cb3d63df
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-02-07 19:14:36 -0300

    Switch.

---
+++ PHP/Estrutura_condicional/switch.php
@@ -0,0 +1,21 @@
+<?php
+  // switch => Estrutura para escolhas ou casos
+  $opcao = 0;
+
+  switch($opcao){
+      case 0:
+         echo "Opção 00";
+             break;
+      case 1:
+         echo "Opção 01";
+             break; 
+      case 2:
+         echo "Opção 02";
+             break;   
+      case 3:
+         echo "Opção 03";
+             break; 
+      default:
+         echo "Opção Inválida";                       
+  }
+?>
