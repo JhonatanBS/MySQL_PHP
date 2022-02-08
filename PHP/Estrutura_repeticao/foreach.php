e1d92b31887be199269a8000c4738ab2f91943da
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-02-08 18:03:44 -0300

    Foreach é um loop usado para manipular arrays.

---
+++ PHP/Estrutura_repeticao/foreach.php
@@ -0,0 +1,8 @@
+<?php
+  // foreach => Usado para para loop de arrays
+  $nomes = ["Mateus", "Lucas", "João", "Paulo"];
+
+  foreach($nomes as $nome){
+    echo "O nome atual é " . $nome . " .<br>";
+  }
+?>
