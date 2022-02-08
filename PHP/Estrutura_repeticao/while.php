e83289a4147c99bf20ef11b40a774dce8b21d2e6
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-02-08 12:32:50 -0300

    Estrutura de repetição usando o while(enquanto).

---
+++ PHP/Estrutura_repeticao/while.php
@@ -0,0 +1,9 @@
+<?php
+  $numero = 0;
+  //Enquanto(condição){código e incremento ou decremento}
+  while($numero < 100){
+      if($numero % 2 != 0)
+        echo $numero . "<br>";
+      $numero++;
+  }
+?>
