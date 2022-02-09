5749b227288d1aeae73dbb7173a02336edbac808
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-02-09 15:50:40 -0300

    Require inclue arquivos e se o arquivo não existir, ele gera um fatal erro.

---
+++ PHP/inclusao/require.php
@@ -0,0 +1,7 @@
+<?php
+  
+  require "require_teste.php";
+//   require "teste.php"; erro porque o arquivo não existe
+?>
+
+<h3>Testando o require</h3>
---
+++ PHP/inclusao/require_teste.php
@@ -0,0 +1,13 @@
+<div>
+    <h1>Require</h1>
+    <p>O require usado para organizar e aproveitar templates.</p>
+</div>
+
+<?php
+
+  $conta = 30000.00;
+
+  echo "Joana tem em sua conta " . $conta . "<br>";
+
+  include __DIR__ . "/../Estrutura_repeticao/loop_aninhado.php";
+?>
