0503f92a4c585e423d82f1e0777443142792b812
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-02-17 21:52:06 -0300

    Separando templates.

--- Blog/contato.php
+++ Blog/contato.php
@@ -1,7 +1,9 @@
 <?php
  include_once "templates/header.php";
 ?>
-    <h1>Olá Mundo!</h1>
+
+<h1>Página de Contato</h1>
+
 <?php
  include_once "templates/footer.php";
 ?>
---
+++ Blog/templates/header.php
@@ -0,0 +1,32 @@
+<?php
+  include_once "helpers/url.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <!-- ESTILOS DO PROJETO -->
+    <link rel="stylesheet" href="<?= $BASE_URL?>/css/styles.css">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,700;1,300&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a href="<?= $BASE_URL ?>" id="logo">
+          <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Blog Codar">
+        </a>
+        <nav>
+            <ul id="navbar">
+                <li><a href="<?= $BASE_URL ?>">Home</a></li>
+                <li><a href="#">Categorias</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="<?= $BASE_URL ?>contato.php">Contatos</a></li>
+            </ul>
+        </nav>
+    </header>
